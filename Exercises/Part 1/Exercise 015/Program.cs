66112f08cdbfbdc6594cc24ab8bc3880f7d2990e
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-05 12:14:36 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give a string:");
+                string meddelande = Console.ReadLine();
+            Console.WriteLine("Give an integer:");
+            string nummer = Console.ReadLine();
+            Console.WriteLine("Give a double:");
+           string decimaltal = Console.ReadLine();
+            Console.WriteLine("Give a boolean:");
+            string boolean = Console.ReadLine();
+            Console.WriteLine("Your string: " + meddelande);
+            Console.WriteLine("Your integer: " + nummer);
+            Console.WriteLine("Your double: " + decimaltal);
+            Console.WriteLine("Your boolean: " + boolean);
+
 
     }
   }
