45f0cfa4babdf52ffc0ffb465b919bdd68701556
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-05 15:05:08 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_21
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give the first number!");
+            string insättning1 = Console.ReadLine();
+             int intvärde1 = Convert.ToInt32(insättning1);
+            Console.WriteLine("Give the second number!");
+            string insättning2 = Console.ReadLine();
+            int intvärde2 = Convert.ToInt32(insättning2);
+            double svar = (intvärde1 + intvärde2) / 2.0;
+            Console.WriteLine("The average is " + svar);
+
+
 
     }
   }
