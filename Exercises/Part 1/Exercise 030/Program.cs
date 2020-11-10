5303b3ec04d0748656763629689994603de72801
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-11 00:26:13 +0200

    11.11.2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,7 +6,41 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give your percent [0 - 100]:");
+            string procent = Console.ReadLine();
+            int procenttal = Convert.ToInt32(procent);
+            if (procenttal <0)
+            {
+            Console.WriteLine("Impossible");
+            }
+            else if (procenttal <=49)
+            {
+            Console.WriteLine("Fail");
+            }
+            else if (procenttal <=59)
+            {
+            Console.WriteLine("Grade: 1");
+            }
+            else if (procenttal <=69)
+            {
+            Console.WriteLine("Grade: 2");
+            }
+            else if (procenttal <=79)
+            {
+            Console.WriteLine("Grade: 3");
+            }
+            else if (procenttal <=89)
+            {
+            Console.WriteLine("Grade: 4");
+            }
+            else if (procenttal <=100)
+            {
+            Console.WriteLine("Grade: 5");
+            }
+            else
+            {
+            Console.WriteLine("Outstanding!");
+            }
 
     }
   }
