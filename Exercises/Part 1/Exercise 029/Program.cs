9fb451877f7033559144cde94c57399fa9f2af9a
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-10 23:52:46 +0200

    10.11.2020

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give the first number!");
+            string nummerinput1 = Console.ReadLine();
+            int nummer1 = Convert.ToInt32(nummerinput1);
+            Console.WriteLine("Give the second number!");
+            string nummerinput2 = Console.ReadLine();
+            int nummer2 = Convert.ToInt32(nummerinput2);
+            if (nummer1 > nummer2)
+            {
+            Console.WriteLine("The larger number is " + nummer1);
+            }
+            else if (nummer1 < nummer2)
+            {
+            Console.WriteLine("The larger number is " + nummer2);
+            }
+            else if (nummer1 == nummer2)
+            {
+            Console.WriteLine("They are equal!");
+            }
 
     }
   }
