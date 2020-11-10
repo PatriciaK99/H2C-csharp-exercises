7d82a27326e11a4ac5991af61d197dcb9b0b73b1
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-10 23:38:03 +0200

    10.11.2020

--- Exercises/Part 1/Exercise 028/Program.cs
+++ Exercises/Part 1/Exercise 028/Program.cs
@@ -6,7 +6,17 @@ namespace exercise_28
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("How old are you?");
+            string nummerinput = Console.ReadLine();
+            int nummer = Convert.ToInt32(nummerinput);
+            if (nummer >=18)
+            {
+            Console.WriteLine("You're an adult!");
+            }
+            else
+            {
+            Console.WriteLine("You're under age!");
+            }
 
     }
   }
