be20bb35383e59f53930347c3e00a330d8c98495
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-17 20:25:25 +0200

    17.11.2020

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,23 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+       int sum = 0;
+    while (true)
+    {
+         Console.WriteLine("Give a number:");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            if (number == 0)
+            {
+            break;
+            }
+            if (number <0)
+            {
+            sum = sum+1;  
+            }
+    }
+         Console.WriteLine("Total amount of negative numbers: " + sum);
+
 
     }
   }
