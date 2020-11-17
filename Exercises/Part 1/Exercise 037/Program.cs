fc6f01cc0b9dc67308e3f92e0db91922278dfbb9
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-17 20:19:37 +0200

    17.11.2020

--- Exercises/Part 1/Exercise 037/Program.cs
+++ Exercises/Part 1/Exercise 037/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_37
     public static void Main(string[] args)
     {
       int sum = 0;
-      // Write your code here:
+    while (true)
+    {
+         Console.WriteLine("Give a number:");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            if (number == 0)
+            {
+            break;
+            }
+            if (number >=1)
+            {
+            sum = sum+1;  
+            }
+    }
+         Console.WriteLine("Total amount of numbers: " + sum);
+
 
     }
   }
