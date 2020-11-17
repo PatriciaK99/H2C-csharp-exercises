8bb86299035821e43001e2bb32e9f288919c22d5
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-17 21:25:43 +0200

    17.11.2020

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -6,8 +6,27 @@ namespace exercise_40
   {
     public static void Main(string[] args)
     {
+      int count = 0;
+      int sum = 0;
+     while (true)
+    {
+      Console.WriteLine("Give a number:");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            if (number == 0)
+            {
+            break;
+            }
+            else
+            {
+            count = count+1;
+            sum = sum + number;
+            }
+           
+    }
+      Console.WriteLine("Total sum of numbers: " + sum);
+      Console.WriteLine("Total amount of numbers: " + count);
 
-      // Write your code here:
     }
   }
 }
