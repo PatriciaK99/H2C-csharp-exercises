09567f2e38c8bb7a8f9c5a8b5eb6081b4352694f
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-18 00:35:02 +0200

    18.11.2020

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -7,8 +7,20 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+            int nummer1 = Convert.ToInt32(Console.ReadLine());
+            int nummer2 = Convert.ToInt32(Console.ReadLine());
+            if (nummer1 > nummer2)
+            {
+            Console.WriteLine(nummer1 +" is grEater than "+ nummer2 + ".");
+            }
+            else if (nummer1 < nummer2)
+            {
+            Console.WriteLine(nummer1 +" is less than "+ nummer2 + ".");
+            }
+            else
+            {
+            Console.WriteLine(nummer1 + " is equal to " + nummer2 + ".");
+            }
     }
   }
 }
