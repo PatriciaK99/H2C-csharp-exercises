f804c8597a851575eb2d24f61b2d1844747979b0
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-17 19:31:18 +0200

    17.11.2020

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+    while (true)
+    {
+      Console.WriteLine("Give a number:");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            if (number < 0)
+            {
+            Console.WriteLine("That is negative");
+            }
+            if (number == 0)
+            {
+            break;
+            }
+            if (number > 0)
+            {
+            Console.WriteLine(number*number);
+            }
+    }
 
     }
   }
