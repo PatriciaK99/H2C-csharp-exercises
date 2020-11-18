60e716da2ef3ed3a5871b108fefc4448f0f8c462
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-11-18 21:05:20 +0200

    18.11.2020

--- Exercises/Part 2/Exercise 045/Program.cs
+++ Exercises/Part 2/Exercise 045/Program.cs
@@ -13,5 +13,6 @@ namespace exercise_45
         Console.WriteLine(count);
                 count++;
     }
+    }
   }
 }
--- Exercises/Part 2/Exercise 046/Program.cs
+++ Exercises/Part 2/Exercise 046/Program.cs
@@ -13,5 +13,6 @@ namespace exercise_46
         Console.WriteLine(count);
                 count++;
     }
+    }
   }
 }
