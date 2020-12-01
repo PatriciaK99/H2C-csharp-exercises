80bab73ef220e08a827a046c15dc8eef58247710
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-12-01 21:17:22 +0200

    1.12.2020

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -6,13 +6,23 @@ namespace exercise_50
   {
     public static void Main(string[] args)
     {
+            Console.WriteLine("How many times");
+         int input = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
       // Call your method here:
+      
+      while (input > count)
+      {
+          PrintPhrase();
+             count ++;
+      }
  
     }
-
-    // Write your method here:
-    public static void PrintPhrase() {
-
+      
+        // Write your method here:
+    public static void PrintPhrase() 
+    {
+            Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
