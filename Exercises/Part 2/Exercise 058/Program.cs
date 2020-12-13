d506341f65a4ca867db25deca96077e7ffde0f00
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-12-13 18:36:58 +0200

    13.12.2020

--- Exercises/Part 2/Exercise 058/Program.cs
+++ Exercises/Part 2/Exercise 058/Program.cs
@@ -4,13 +4,25 @@ namespace exercise_58
 {
   class Program
   {
+
+    public static int Smallest(int number1, int number2) 
+    {
+        // write your code here
+        // do not print anything inside the method
+
+        // there must be a return command at the end
+       int answer = number1;
+      return answer;
+    }
+
+
     public static void Main(String[] args)
     {
       int answer = Smallest(2, 7);
       Console.WriteLine("Smallest: " + answer);
     }
 
-    // Write your method here:
+  
     
   }
 }
--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -4,13 +4,23 @@ namespace exercise_59
 {
   class Program
   {
+        public static int Greatest(int number1, int number2, int number3) 
+{
+         // write your code here
+         // do not print anything inside the method
+
+         // there must be a return command at the end
+         int answer = number2;
+            return answer;
+}
+
     public static void Main(String[] args)
     {
       int answer = Greatest(2, 7, 3);
       Console.WriteLine("Greatest: " + answer);
     }
 
-    // Write your method here:
+   
 
   }
 }
