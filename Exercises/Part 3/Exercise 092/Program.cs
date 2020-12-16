a27c72b65a8875b8357215b02daf494b6f508058
Camilla Kroksjö <camilla.kroksjo@centria.fi>
2020-12-16 23:32:43 +0200

    16.12.2020

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,6 +7,39 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+            int highest = 0;
+            int longest = 0;
+            string longestname = "";
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input != "")
+                {
+                    string [] nameandage = input.Split(',');
+                    int year = Convert.ToInt32(nameandage[1]);
+                    int namesize = nameandage[0].Length;
+                    int age = 2020 - (year);
+
+                    if (age > highest) highest = age;
+
+                    if (namesize > longest)
+                    {
+                        longest = namesize;
+                        longestname = nameandage[0];
+                    }
+
+                
+                }
+
+                else
+                {
+                    Console.WriteLine("Longest name: " + longestname);
+                    Console.WriteLine("Highest age: " + highest);
+                
+                }
+            
+            }
 
     }
   }
